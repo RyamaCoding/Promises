7e1848da9d3a334214a436d2038c244d4a7f886f
Raghu Rayamajhi <ryamacoding@outlook.com>
2023-10-19 14:36:58 +0100

    fixing some stuff

--- app.js
+++ app.js
@@ -1,48 +1,95 @@
-// 1. Then
+// // 1. Then
+// const emailRef = document.querySelector(".email");
+
+// const statusRef = document.querySelector(".status")
+
+// const videoRef = document.querySelector(".video")
+
+// // console.log(emailRef);
+
+// // fetch("https://jsonplaceholder.typicode.com/users/1")
+// //   .then((response) => {
+// //     return response.json();
+// //   })
+// //   .then((data) => {
+// //     console.log(data);
+// //     emailRef.innerHTML = data.email
+// //   });
+
+// // 2. Async/await
+
+// async function main() {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+//     const data = await response.json()
+//     console.log(data)
+//     emailRef.innerHTML = data.email
+// }
+
+// main();
+
+// //new promise
+
+// function getSubscriptionStatus() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve("FREE")
+//         }, 2000)
+//     })
+// }
+
+// //challenge 23:33 includes subscription status from above
+
+// function getVideo(subscriptionStatus) {
+//     return new Promise((resolve, reject) => {
+//         if(subscriptionStatus === "VIP") {
+//             resolve("show video")
+//         }
+//         else if(subscriptionStatus === "FREE") {
+//             resolve("show trailer")
+//         }
+//         else{
+//             reject("no video")
+//         }
+//     })
+// }
+
+// async function main() {
+//     const status = await getSubscriptionStatus();
+//     statusRef.innerHTML = status
+//     try {
+//         console.log(await getVideo(status))
+//     }
+//     catch (e) {
+//         console.log(e)
+//         videoRef.innerHTML = e;
+//     }
+// }
+
+// main();
+
 const emailRef = document.querySelector(".email");
+const statusRef = document.querySelector(".status");
+const videoRef = document.querySelector(".video");
 
-const subRef = document.querySelector(".sub")
-
-const statusRef = document.querySelector(".sub")
-
-const videoRef = document.querySelector(".video")
-
-console.log(emailRef);
-
-fetch("https://jsonplaceholder.typicode.com/users/1")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
+async function main1(){
+    const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
+    const data = await response.json();
     console.log(data);
-    emailRef.innerHTML = data.email
-  });
-
-// 2. Async/await
-
-async function main() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
-    const data = await response.json()
-    console.log(data)
-    emailRef.innerHTML = data.email
+    emailRef.innerHTML = data.email;
 }
 
-main();
+main1 ();
 
-//new promise
-
-function getSubscriptionStatus() {
-    return new Promise((resolve, reject) => {
+function getSubscriptionStatus(){
+    return new Promise ((resolve, reject) => {
         setTimeout(() => {
-            resolve(undefined)
-        }, 2000)
-    })
+            resolve("VIP");
+        }, 2000);
+    });
 }
 
-//challenge 23:33 includes subscription status from above
-
-function getVideo(subscriptionStatus) {
-    return new Promise((resolve, reject) => {
+function getVideo(subscriptionStatus){
+    return new Promise ((resolve, reject) => {
         if(subscriptionStatus === "VIP") {
             resolve("show video")
         }
@@ -57,7 +104,7 @@ function getVideo(subscriptionStatus) {
 
 async function main() {
     const status = await getSubscriptionStatus();
-    statusRef.innerHTML = status
+    statusRef.innerHTML = status;
     try {
         console.log(await getVideo(status))
     }
@@ -67,4 +114,4 @@ async function main() {
     }
 }
 
-main();
+main ();
